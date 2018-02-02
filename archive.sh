353790bda60b13d37ccd9101728c4d21ce8d0e93
Ethan White <ethan@weecology.org>
2018-02-02 15:23:22 -0500

    Use the same token for push and release

--- archive.sh
+++ archive.sh
@@ -13,4 +13,4 @@ git push --quiet deploy master > /dev/null 2>&1
 current_date=`date -I | head -c 10`
 git tag $current_date
 git push --quiet deploy --tags > /dev/null 2>&1
-curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_TOKEN" https://api.github.com/repos/ethanwhite/travistest/releases -d "{\"tag_name\":\"$current_date\"}"
+curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/ethanwhite/travistest/releases -d "{\"tag_name\":\"$current_date\"}"
