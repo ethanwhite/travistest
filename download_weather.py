1aae7dd26cee3f07c0f8ccc04dee725c8da4e420
Ethan White <ethan@weecology.org>
2019-01-02 14:34:21 -0500

    Try using docker to run download_weather.py
    Because why not, I mean really

--- download_weather.py
+++ download_weather.py
@@ -5,4 +5,4 @@ url = 'http://166.153.133.121/?command=TableDisplay&table=MET&records=10'
 r = requests.get(url, stream=True, timeout = 30)
 
 for line in r.iter_lines():
-    if line: print line
+    if line: print(line)
