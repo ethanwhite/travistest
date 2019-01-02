c6973805df8ac0856aaef0789d116c7edb5846e0
Ethan White <ethan@weecology.org>
2019-01-02 14:06:44 -0500

    Try explicitly timing out

--- download_weather.py
+++ download_weather.py
@@ -1,5 +1,5 @@
 import requests
 
 url = 'http://166.153.133.121/?command=TableDisplay&table=MET&records=10'
-r = requests.get(url, allow_redirects=True)
+r = requests.get(url, allow_redirects=True, timeout=30)
 open('weather.txt', 'wb').write(r.content)
