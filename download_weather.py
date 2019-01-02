69ec55d6790d298a9f889e6c31c5182c232a83b3
Ethan White <ethan@weecology.org>
2019-01-02 14:17:41 -0500

    Try streaming the data

--- download_weather.py
+++ download_weather.py
@@ -1,5 +1,8 @@
 import requests
 
 url = 'http://166.153.133.121/?command=TableDisplay&table=MET&records=10'
-r = requests.get(url, allow_redirects=True, timeout=30)
-open('weather.txt', 'wb').write(r.content)
+
+r = requests.get(url, stream=True, timeout = 30)
+
+for line in r.iter_lines():
+    if line: print line
